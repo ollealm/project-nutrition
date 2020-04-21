ff25452aac3b48428f82b83f7e1b94d8035cec52
Van <vango85@gmail.com>
2020-04-21 22:23:15 +0200

    [xx] Added input field for testing with example code

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import { BarcodeScanner } from "components/BarcodeScanner";
+
+const onDetected = (code) => {
+  console.log(`Code: ${code}`);
+  fetch(`https://world.openfoodfacts.org/api/v0/product/${code}.json`)
+    .then((data) => data.json())
+    .then((json) => {
+      console.log(json);
+    });
+};
 
 export const App = () => {
   return (
     <div>
-      Find me in src/app.js!
+      <label>
+        {" "}
+        Test codes here:{" "}
+        <input type="text" onChange={(e) => onDetected(e.target.value)}></input>
+      </label>
+      <p>
+        {" "}
+        Use the field above to test barcodes manually and keep an eye on your
+        console in the browser. i.e. Type 7311070347272 - Pågen Gifflar. Yum
+      </p>
+      <BarcodeScanner onDetected={onDetected}></BarcodeScanner>
     </div>
-  )
-}
+  );
+};
--- code/src/components/BarcodeScanner.js
+++ code/src/components/BarcodeScanner.js
@@ -1,42 +1,45 @@
-import React, { useRef, useState, useLayoutEffect } from 'react'
-import Quagga from 'quagga'
+import React, { useRef, useState, useLayoutEffect } from "react";
+import Quagga from "quagga";
 
 export const BarcodeScanner = ({ className, onDetected }) => {
-  const [initializing, setInitializing] = useState(true)
-  const cameraDivRef = useRef()
+  const [initializing, setInitializing] = useState(true);
+  const cameraDivRef = useRef();
 
   Quagga.onDetected((data) => {
-    onDetected(data.codeResult.code)
-  })
+    onDetected(data.code);
+  });
 
   useLayoutEffect(() => {
-    Quagga.init({
-      inputStream: {
-        name: 'Live',
-        type: 'LiveStream',
-        target: cameraDivRef.current
+    Quagga.init(
+      {
+        inputStream: {
+          name: "Live",
+          type: "LiveStream",
+          target: cameraDivRef.current,
+        },
+        decoder: {
+          readers: ["ean_reader"],
+        },
       },
-      decoder: {
-        readers: ['ean_reader']
+      (err) => {
+        if (err) {
+          console.error("Failed to initialize reader", err);
+          return;
+        }
+        Quagga.start();
+        setInitializing(false);
       }
-    }, (err) => {
-      if (err) {
-        console.error('Failed to initialize reader', err)
-        return
-      }
-      Quagga.start()
-      setInitializing(false)
-    })
+    );
 
     return () => {
-      Quagga.stop()
-    }
-  }, [])
+      Quagga.stop();
+    };
+  }, []);
 
   return (
     <>
       {initializing && <div>Starting camera...</div>}
       <div ref={cameraDivRef} className={className} />
     </>
-  )
-}
+  );
+};
